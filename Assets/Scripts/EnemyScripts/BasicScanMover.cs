0b7d4537bedadd2b4a4d5f1da6fecd805330cd60
VedakTheDev <zwjclaxton@gmail.com>
2019-09-21 13:00:43 -0500

    First of the Bugaroos!
    The movement mechanic is in and it does a thing! There is also a suppper basic death function. Should be updated in the future!

---
+++ Assets/Scripts/EnemyScripts/BasicScanMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicScanMover : MonoBehaviour
+{
+	private Transform bugTrans;
+	private BasicBugBehavior bug;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		bugTrans = transform.parent.GetChild(0).GetComponent<Transform>();
+		bug = transform.parent.GetChild(0).GetComponent<BasicBugBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = bugTrans.position;
+		transform.up = bug.getTransformUp();
+    }
+}
